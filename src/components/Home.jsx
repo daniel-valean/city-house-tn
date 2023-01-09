00591653c4682305aab4a745d8498bb7fa844638
daniel-valean <dannyvalean@gmail.com>
2023-01-09 15:22:01 -0800

    landingpage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import {} from 'react-scroll'
 import videobg from '../assets/home.mp4'
 
 const Home = () => {
   return (
-    <div name='Home' className='home' video src>
+    <div className='home'>
         <video src={videobg} autoPlay loop muted/>
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <h1 className='text-4xl sm:text-7xl font-bold text-white'>
-              Daniel Valean</h1>
-        </div>
+      <div className='content-buy'>
+          <h1>We buy houses as-is, in middle Tennessee</h1>
+      </div>
     </div>
   )
 }
