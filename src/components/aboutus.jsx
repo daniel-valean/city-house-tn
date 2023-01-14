9d79eb2cd25a929ca0bdea93b15448e051dd4744
daniel-valean <dannyvalean@gmail.com>
2023-01-14 02:12:31 -0700

    commented out imports on aboutus

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import Html from '../assets/html.png';
-import Mongo from '../assets/mongo.png';
+// import CSS from '../assets/css.png';
+// import JavaScript from '../assets/javascript.png';
+// import ReactImg from '../assets/react.png';
+// import Node from '../assets/node.png';
+// import Html from '../assets/html.png';
+// import Mongo from '../assets/mongo.png';
 
 
 
@@ -17,7 +17,30 @@ const aboutus = () => {
                 <p className='text-5xl font-bold inline border-b-4 border-[#aacded]'>About Us</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-
+            {/* <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={Html} alt="HTML icon"/>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={CSS} alt="CSS icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={JavaScript} alt="JAVASCRIPT icon" />
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={ReactImg} alt="REACT icon" />
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={Node} alt="NODE icon" />
+                    <p className='my-4'>NODE</p>
+                </div>
+                <div className='hover:scale-110 duration-500'>
+                    <img className='w-40 mx-auto' src={Mongo} alt="MONGO icon" />
+                    <p className='my-4'>MONGO</p>
+                </div> */}
             </div>
         </div>
     </div>
