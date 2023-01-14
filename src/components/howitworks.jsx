1bfa3a73ec2ec1688460c901308828e2ffb6578d
daniel-valean <dannyvalean@gmail.com>
2023-01-14 15:51:26 -0700

    how it works info filled in

--- src/components/howitworks.jsx
+++ src/components/howitworks.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 
 const howitworks = () => {
   return (
-    <div name='howitworks' className='w-full h-screen bg-[#b1a296ab] text-white'>
+    <div name='howitworks' className='w-full h-screen bg-[#9696b1ab] text-white'>
         <div className='flex flex-col w-full h-full'>
-            <div className='max-w-[1000px] w-full px-4 mt-10'>
+            <div className='max-w-full w-full px-4 mt-20 max-w-'>
                 <div className='ml-28 pb-8 pl-4'>
-                    <h1 className='text-5xl font-thin border-b-4 border-[#ffffff]'>We Make It Easy To Sell Your House Fast</h1>
-                    <p className='text-2xl font-thin mt-10'>Step 1 - Free Evaluation</p>
-                    <p className='text-2xl font-thin mt-10'>Step 2 - Cash Offer</p>
-                    <p className='text-2xl font-thin mt-10'>Step 3 - Agreement</p>
-                    <p className='text-2xl font-thin mt-10'>Step 4 - Closing</p>
+                    <h1 className='text-6xl font-thin'>We Make It Easy To Sell Your House Fast</h1>
+                    <p className='max-w-6xl text-4xl font-bold mt-11'>Step 1 - Free Evaluation</p>
+                    <p className='max-w-full text-2xl font-thin mt-6'>After we receive your information we will schedule a time to visit with you and evaluate the condition of your home.</p>
+
+                    <p className='max-w-6xl text-4xl font-bold mt-11'>Step 2 - Cash Offer</p>
+                    <p className='max-w-full text-2xl font-thin mt-6'>We usually make a cash offer on the spot - otherwise you will receive a cash offer within 24 hours.</p>
+
+                    <p className='max-w-6xl text-4xl font-bold mt-11'>Step 3 - Agreement</p>
+                    <p className='max-w-full text-2xl font-thin mt-6'>If you find the offer acceptable, we will prepare a standard Tennessee real estate sales contract for the sale of your home.</p>
+
+                    <p className='max-w-6xl text-4xl font-bold mt-11'>Step 4 - Closing</p>
+                    <p className='max-w-full text-2xl font-thin mt-6'>We will then visit the title company so they can complete the title search and schedule a day and time for closing.</p>
+
                 </div>
             </div>
         </div>
