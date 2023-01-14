3d8a48d6ab26431e912ab5a1e3f553817ef50855
daniel-valean <dannyvalean@gmail.com>
2023-01-14 03:01:37 -0700

    about us bug, added ;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./components/Home";
 import Navbar from "./components/navbar";
-import Howitworks from "./components/howitworks"
+import Howitworks from "./components/howitworks";
 import Aboutus from "./components/Aboutus";
 
 
