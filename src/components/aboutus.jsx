caede2804b87e0ca305c223a5a0ee084be219103
daniel-valean <dannyvalean@gmail.com>
2023-01-15 02:07:56 -0700

    about us family photo and writing

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
+import Family from '../assets/family-1.jpg'
 
 
 const aboutus = () => {
 return (
-    <div name='aboutus' className='w-full h-screen bg-[#a9a9af] text-white'>
+    <div name='aboutus' className='w-full h-screen bg-[#868689] text-white'>
     {/* container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
-                <p className='text-5xl font-bold inline border-b-4 border-[#aacded]'>About Us</p>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col w-full h-full'>
+            <div className='mt-20'>
+                <p className='text-6xl font-thin inline border-b-4 border-[#aacded]'>About Us</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-            {/* <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={Html} alt="HTML icon"/>
-                    <p className='my-4'>HTML</p>
-                </div>
-                <div className='hover:scale-110 duration-500'>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2'>
+            <div>
+                    <img className='w-40 mt-10 ml-8' src={Family} alt="Family Pic"/>
+            </div>
+            <div className='font-thin mt-10 w-full'>                    
+                <p className=''>We love Nashville and we love real estate! We have helped may home owners with difficult properties and situations, 
+                weather they needed costly repairs, owed back taxes, inherited a problem house, or were in a tough financial situation.
+                At Cityhouse Properties we believe that we are successful because we make the selling process easy for any home seller. 
+                Every transaction is transparent, fair, and fast! No commission, no fees, no repairs, and close in as quick as 7 days.
+                Call us for a no-obligations cash offer on your house!</p>
+            </div>
+                {/* <div className='hover:scale-110 duration-500'>
                     <img className='w-40 mx-auto' src={CSS} alt="CSS icon" />
                     <p className='my-4'>CSS</p>
-                </div>
-                <div className='hover:scale-110 duration-500'>
+                </div> */}
+                {/* <div className='hover:scale-110 duration-500'>
                     <img className='w-40 mx-auto' src={JavaScript} alt="JAVASCRIPT icon" />
                     <p className='my-4'>JAVASCRIPT</p>
                 </div>
