8e971c80e5cd0c11e13f1dd18d471c9823abb955
daniel-valean <dannyvalean@gmail.com>
2023-01-15 21:57:35 -0500

    about us icons

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Family from '../assets/family-1.jpg'
+import Caldwell1 from '../assets/calwell-before.jpg';
+import Cashoffer from '../assets/cashoffer.png';
+import Agreement from '../assets/agreement.png';
+import Closed from '../assets/closed.png';
 
 
 const aboutus = () => {
 return (
-    <div name='aboutus' className='w-full h-screen bg-[#868689] text-white'>
+    <div name='aboutus' className='w-full h-screen bg-[#ffffff] text-black'>
     {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col w-full h-full'>
             <div className='mt-20'>
-                <p className='text-6xl font-thin inline border-b-4 border-[#aacded]'>About Us</p>
+                <p className='text-5xl font-thin inline border-b-4 border-[#aacded]'>About Us</p>
             </div>
-            <div className='max-w-[1000px] w-full grid sm:grid-cols-2'>
+            <div className='max-w-[1000px] w-full mb-10 grid sm:grid-cols-2'>
             <div>
                     <img className='w-40 mt-10 ml-8' src={Family} alt="Family Pic"/>
             </div>
@@ -21,26 +25,34 @@ return (
                 Every transaction is transparent, fair, and fast! No commission, no fees, no repairs, and close in as quick as 7 days.
                 Call us for a no-obligations cash offer on your house!</p>
             </div>
-                {/* <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={CSS} alt="CSS icon" />
-                    <p className='my-4'>CSS</p>
-                </div> */}
-                {/* <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={JavaScript} alt="JAVASCRIPT icon" />
-                    <p className='my-4'>JAVASCRIPT</p>
+        </div>
+
+
+            <div className='bg-grey-400'>
+                <p className='text-5xl font-thin inline border-b-4 border-[#dcdcdc]'>Some of Our Work</p>
+            </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-2 gap-4 text-center py-8'>
+            <div className='border'>
+                    <img className='w-40 mx-auto' src={Caldwell1} alt="Evaluation Icon"/>
+                    <p className='my-4 text-2xl'>Step 1 - Free Evaluation</p>
                 </div>
-                <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={ReactImg} alt="REACT icon" />
-                    <p className='my-4'>REACT</p>
+
+                <div className='border border-[#000000] rounded-md bg-[#7575758f] text-white'>
+                    <img className='w-40 mx-auto' src={Cashoffer} alt="Cash Offer Icon"/>
+                    <p className='my-4 text-2xl font-thin'>Step 2 - Cash Offer</p>
                 </div>
+
                 <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={Node} alt="NODE icon" />
-                    <p className='my-4'>NODE</p>
+                    <img className='w-40 mx-auto' src={Agreement} alt="Agreement Icon"/>
+                    <p className='my-4 text-2xl'>Step 3 - Agreement</p>
                 </div>
+
                 <div className='hover:scale-110 duration-500'>
-                    <img className='w-40 mx-auto' src={Mongo} alt="MONGO icon" />
-                    <p className='my-4'>MONGO</p>
-                </div> */}
+                    <img className='w-40 mx-auto' src={Closed} alt="Closed"/>
+                    <p className='my-4 text-2xl'>Step 4 - Closed</p>
+                </div>
+
+
             </div>
         </div>
     </div>
