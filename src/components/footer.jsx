6916d46d4257203e7772062f3b9beb01e99cd812
daniel-valean <dannyvalean@gmail.com>
2023-01-24 16:23:48 -0700

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class footer extends Component {
   render() {
     return (
-      <div>footer</div>
+      <div className='bg-[#39393b8c] text-white justify-center h-[80px]'>
+        <h3> </h3>
+      </div>
     )
   }
 }
