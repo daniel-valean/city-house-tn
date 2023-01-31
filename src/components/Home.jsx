aa6ca729cd4e750e72f6576ae571961ded312eb8
daniel-valean <dannyvalean@gmail.com>
2023-01-31 00:53:59 -0700

    fixed homepage to be responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import videobg from "../assets/home.mp4";
+
+import background from "../assets/homepage-5.jpg";
 
 const Home = () => {
   return (
 
-    <div name="Home" className="w-full h-screen">
-      <video src={videobg} autoPlay loop muted playsInLine id="video" />
+    <div name='Home' className='w-full h-screen' style={{ background:`url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
       {/* <div className='bg-overlay'></div> */}
       {/* container */}
-      <div
-        className="max-w-[1000px] text-center ml-8 md:ml-20 mt-28 md:mt-60 lg:pl-80 lg:mt-20 lg h-full" id="writing">
+      <div className="max-w-[1000px] mx-auto text-center ml-10 mt-36 md:ml-20 lg:ml-0 lg:mt-0 lg:text-left lg:px-8 flex flex-col lg:justify-center h-full lg:pl-28" id="writing">
         <h1 className="text-4xl sm:text-7xl text-white font-thin">
           We Buy Houses
         </h1>
@@ -27,36 +26,18 @@ const Home = () => {
 
       <div
         name="form"
-        className="w-full h-screen mt-80 md:pt-80 md:pl-28 lg:ml-80 lg:mt-24 "
+        className="w-full h-screen flex items-center justify-center mt-48 lg:mt-6 lg:justify-end lg:p-10 "
         id="form">
         <form
           method="POST"
           action="https://getform.io/f/560c6240-58a7-4478-ba2c-ee4ddb075957"
-          className="flex flex-col max-w-[600px] border-2 bg-gray-400/50 shadow-md rounded px-8">
-          <input
-            className="bg-white mt-4 p-2"
-            type="text"
-            placeholder="Property Address"
-            name="address"/>
-          <input
-            className="my-4 p-2 bg-white"
-            type="text"
-            placeholder="Full Name"
-            name="name"/>
-          <input
-            className="bg-white p-2"
-            type="number"
-            placeholder="Phone Number"
-            name="number"/>
-          <input
-            className="my-4 p-2 bg-white"
-            type="email"
-            placeholder="Email"
-            name="email"/>
+          className=" md:w-full flex flex-col max-w-[600px] border-2 bg-gray-400/50 shadow-md rounded px-10 md:px-10">
+          <input className="bg-white mt-4 p-3" type="text" placeholder="Property Address" name="address"/>
+          <input className="my-4 p-3 bg-white" type="text" placeholder="Full Name" name="name"/>
+          <input className="bg-white p-3" type="number" placeholder="Phone Number" name="number"/>
+          <input className="my-4 p-3 bg-white" type="email" placeholder="Email" name="email"/>
 
-          <button className="text-white border-2 hover:border-[#282421a9] hover:bg-[#282421a9] px-4 py-3 my-8 mx-auto flex items-center">
-            Get My Cash Offer
-          </button>
+          <button className="text-white border-2 hover:border-[#282421a9] hover:bg-[#282421a9] px-4 py-3 my-8 mx-auto flex items-center">Get My Cash Offer</button>
         </form>
       </div>
 
